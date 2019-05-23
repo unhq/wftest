992f1f6c36aafc5570cc230d350b20463498ee50
Ace <dimitrievski@un.org>
2019-05-23 13:09:28 -0400

    test site initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>UN Web site 1</title>
+  <meta name="description" content="UN site 1">
+  <meta name="author" content="UN">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+</head>
+
+<body>
+    <h1>I'm heading 1 and css will make me blue</h1>
+    Let PHP fetch your password, will show below this line:<br />
+    <?php
+        include 'settings.php';
+        echo $password;
+    ?>
+</body>
+</html>
